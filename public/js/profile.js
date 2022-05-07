081acbe91475b0319a591c3630af1fdefa2ac0b5
Chuan Wang <chuan.wang101@gmail.com>
2022-05-06 22:00:51 -0700

    blogs can now be deleted

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,21 +1,35 @@
-console.log("hello")
-document.querySelector("#newBlog").addEventListener("submit",e=>{
+document.querySelector("#newBlog").addEventListener("submit", e => {
     e.preventDefault()
     const blogObj = {
-        title:document.querySelector("#title").value,
-        body:document.querySelector("#body").value,
+        title: document.querySelector("#title").value,
+        body: document.querySelector("#body").value,
     }
-    fetch("/api/blogs",{
-        method:"POST",
-        body:JSON.stringify(blogObj),
-        headers:{
-            "Content-Type":"application/json"
+    fetch("/api/blogs", {
+        method: "POST",
+        body: JSON.stringify(blogObj),
+        headers: {
+            "Content-Type": "application/json"
         }
-    }).then(res=>{
-        if(res.ok){
-           location.reload()
+    }).then(res => {
+        if (res.ok) {
+            location.reload()
         } else {
-            alert("trumpet sound")
+            alert("post failed")
         }
     })
 })
+
+const buttons = document.querySelectorAll(".deleteBtn")
+for (const button of buttons) {
+    button.addEventListener('click', e => {
+        fetch(`/api/blogs/${e.target.value}`, {
+            method: "DELETE"
+        }).then(res => {
+            if (res.ok) {
+                location.reload()
+            } else {
+                alert("delete failed")
+            }
+        })
+    })
+}
